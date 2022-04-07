0b63f147c42d8687742c31cb5468c35e96d91ac2
Ilyh Kalinin <ilyapochta0118@mail.ru>
2022-04-07 10:06:56 +0300

    splitting_popups

--- scripts/script.js
+++ scripts/script.js
@@ -28,92 +28,39 @@ const initialCards = [
   }
   ];
 // элементы в Profile
-const profileEditButton = page.querySelector('.profile__info-edit');
-const profileAddCardButton = page.querySelector('.profile__add-btn');
-const profileName = page.querySelector('.profile__author-name');
-const profileAbout = page.querySelector('.profile__about-author');
-// элементы в Popup
-const popup = page.querySelector('.popup');
-const popupForm = popup.querySelector('.popup__form');
-const popupContainer = popup.querySelector('.popup__container');
-const popupHeading = popup.querySelector('.popup__heading');
-const popupToggleButton = popup.querySelector('.popup__toggle');
-const popupNameItem = popup.querySelector('.popup__item_el_name');
-const popupAboutItem = popup.querySelector('.popup__item_el_about');
-const popupButton = popup.querySelector('.popup__button');
-const popupFigure = popup.querySelector('.popup__figure');
-const popupFigureImage = popup.querySelector('.popup__figure-img');
-const popupFigureCaption  = popup.querySelector('.popup__figure-caption');
+const profileSection = page.querySelector('.profile');
+const profileEditButton = profileSection.querySelector('.profile__info-edit');
+const profileAddCardButton = profileSection.querySelector('.profile__add-btn');
+const profileName = profileSection.querySelector('.profile__author-name');
+const profileAbout = profileSection.querySelector('.profile__about-author');
+// profiele-popup и его элементы
+const profilePopup = page.querySelector('.profile-popup');
+const profilePopupForm = profilePopup.querySelector('.popup__form');
+const profilePopupToggleButton = profilePopup.querySelector('.popup__toggle');
+const profilePopupNameItem = profilePopup.querySelector('.popup__item_el_name');
+const profilePopupAboutItem = profilePopup.querySelector('.popup__item_el_about');
+// place-popup
+const placePopup = page.querySelector('.place-popup');
+const placePopupForm = placePopup.querySelector('.popup__form');
+const placePopupToggleButton = placePopup.querySelector('.popup__toggle');
+const placePopupNameItem = placePopup.querySelector('.popup__item_el_name');
+const placePopupAboutItem = placePopup.querySelector('.popup__item_el_about');
+// figure-popup
+const figurePopup = page.querySelector('.figure-popup');
+const figurePopupToggleButton = figurePopup.querySelector('.popup__toggle');
+const figurePopupImage = figurePopup.querySelector('.popup__figure-img');
+const figurePopupCaption  = figurePopup.querySelector('.popup__figure-caption');
 
-function showPopup() {
-  popup.classList.add('popup_opened');
-}
 
-function hidePopup(){
-  popup.classList.remove('popup_opened');
-}
-
-function hideElement(element) {
-  element.classList.add('hide');
-}
-
-function showElement(element) {
-  element.classList.remove('hide');
-}
-
-function setEditPopup() {
-  popupHeading.innerText = 'Редактировать профиль';  
-  popupNameItem.value = profileName.innerText;
-  popupAboutItem.value = profileAbout.innerText;
-  popupButton.type = 'submit';
-  popupButton.innerText = 'Сохранить';
-  popupContainer.classList.remove('popup__container_content_figure');
-  popup.classList.remove('popup_content_figure')
-  hideElement(popupFigure);
-  showElement(popupHeading);
-  showElement(popupForm);
-}
-
-function setAddPopup() {
-  popupHeading.innerText = 'Новое место';
-  popupNameItem.value = '';
-  popupAboutItem.value = '';
-  popupNameItem.placeholder = 'Название';
-  popupAboutItem.placeholder = 'Ссылка на картинку';
-  popupButton.type = 'submit';
-  popupButton.innerText = 'Создать';
-  popupContainer.classList.remove('popup__container_content_figure');
-  popup.classList.remove('popup_content_figure')
-  hideElement(popupFigure);
-  showElement(popupHeading);
-  showElement(popupForm);
-};
-
+// объявления функций
 function setFigurePopup(eventItem) {
   const newCardsItem = eventItem.target.parentElement;
-  const url = eventItem.target.style.backgroundImage.slice(5,-2);
-  popupFigureImage.src = url;
-  popupFigureImage.alt = newCardsItem.querySelector('.cards__title').textContent;
-  popupFigureCaption.textContent = popupFigureImage.alt;
-  popupContainer.classList.add('popup__container_content_figure');
-  popup.classList.add('popup_content_figure');
-  showElement(popupFigure);
-  hideElement(popupHeading);
-  hideElement(popupForm);
+  const url = newCardsItem.querySelector('.cards__image').style.backgroundImage.slice(5,-2);
+  const caption = newCardsItem.querySelector('.cards__title').textContent;
+  figurePopupImage.src = url;
+  figurePopupImage.alt = caption;
+  figurePopupCaption.textContent = caption;
 };
-
-function callPopup(evt){
-  if (evt.target.className.includes('profile__info-edit')) {
-    setEditPopup();
-  }
-  else if(evt.target.className.includes('profile__add-btn')) {
-    setAddPopup();
-  }
-  else {
-    setFigurePopup(evt);
-  }
-  showPopup();
-}
 
 function fillTemplate(name, link) {
   const cardTemplate = page.querySelector('#card-template').content;
@@ -124,16 +71,19 @@ function fillTemplate(name, link) {
   const cardRemoveButton = card.querySelector('.cards__remove-button');
   cardImage.style.backgroundImage = `url(${link})`;
   cardTitle.textContent = name;
-  cardLikeButton.addEventListener('click', evt => {
-    evt.target.classList.toggle('cards__like-button_active');
-  });
-  cardRemoveButton.addEventListener('click', evt => {
-    evt.target.closest('.cards__item').remove();
-  });
-  cardImage.addEventListener('click', evt => {
-    callPopup(evt);
-  });
+  cardLikeButton.addEventListener('click', evt => evt.target.classList.toggle('cards__like-button_active'));
+  cardRemoveButton.addEventListener('click', evt => evt.target.closest('.cards__item').remove());
+  cardImage.addEventListener('click', evt => setFigurePopup(evt));
   return card;
+}
+
+function createCard(initCard) {
+  if (initCard) {
+    return fillTemplate(initCard.name, initCard.link);
+  } 
+  else {
+    return fillTemplate(placePopupNameItem.value, placePopupAboutItem.value);
+  }  
 }
 
 function insertCard(card) {
@@ -142,33 +92,60 @@ function insertCard(card) {
 
 function initialAddCards() {
   initialCards.forEach(elem => {
-    const newCard = fillTemplate(elem.name, elem.link);
+    const newCard = createCard(elem);
     insertCard(newCard);
   });
 }
 
-function addCard(){
-  const newCard = fillTemplate(popupNameItem.value, popupAboutItem.value);
-  insertCard(newCard);
-  hidePopup();
+function callProfileSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = profilePopupNameItem.value;
+  profileAbout.textContent = profilePopupAboutItem.value;
 }
 
-function callSubmitHandler (evt) {
+function callPlaceSubmitHandler (evt) {
   evt.preventDefault();
-  if (popupButton.innerText === 'Сохранить') { 
-    profileName.textContent = popupNameItem.value;
-    profileAbout.textContent = popupAboutItem.value;
-  }  
-  else{
-    addCard();
-  }
-  hidePopup();
+  const newCard = createCard();
+  insertCard(newCard);
+}
+
+function setProfilePopup() {
+  profilePopupNameItem.value = profileName.innerText;
+  profilePopupAboutItem.value = profileAbout.innerText;
+}
+
+function showPopup(item) {
+  console.log(item)
+  item.classList.add('popup_opened');
+}
+
+function hidePopup(item) {
+  item.classList.remove('popup_opened');
+}
+
+function getCurrentPopup(item) {
+  return item.target.closest('.popup');
 }
 
 initialAddCards();
 
-profileEditButton.addEventListener('click', callPopup);
-profileAddCardButton.addEventListener('click', callPopup);
-popupToggleButton.addEventListener('click', hidePopup);
-popupForm.addEventListener('submit', callSubmitHandler);
-
+// обработчики секции profile кнопок
+profileEditButton.addEventListener('click', evt => {
+  setProfilePopup();
+  showPopup(profilePopup);
+});
+profileAddCardButton.addEventListener('click', elem => showPopup(placePopup));
+// обработчики profile-popup
+profilePopupToggleButton.addEventListener('click', elem => hidePopup(getCurrentPopup(elem)));
+profilePopupForm.addEventListener('submit', elem => {
+  callProfileSubmitHandler(elem);
+  hidePopup(getCurrentPopup(elem));
+});
+// обработчики place-popup
+placePopupToggleButton.addEventListener('click', elem => hidePopup(getCurrentPopup(elem)));
+placePopupForm.addEventListener('submit', elem => {
+  callPlaceSubmitHandler(elem);
+  hidePopup(getCurrentPopup(elem));
+});
+// обработчики place-popup
+figurePopupToggleButton.addEventListener('click', elem => hidePopup(getCurrentPopup(elem)));
